4422156c637627da1cd524bdc5fea57f05366523
oMegaPB <96842602+oMegaPB@users.noreply.github.com>
2022-12-26 20:53:44 +0300

    Update AESqlite.py

--- AESqlite.py
+++ AESqlite.py
@@ -1,23 +1,23 @@
 import base64
 import sqlite3
 import typing as t
-from enum import IntEnum
 from hashlib import md5
+from enum import IntEnum
 
 from Crypto.Cipher import AES
 
 xInputDataT = t.Union[t.List[t.Dict[str, t.Any]], t.Dict[str, t.Any]]
 xResponseValueT = t.Optional[t.Union[t.List[t.Dict[str, t.Any]], t.Dict[str, t.Any], int]]
 
-class DataBaseException(Exception):
-    pass
-
 class FetchMode(IntEnum):
     FETCH_ONE = 1
     FETCH_ALL = 2
 
+class DataBaseException(Exception):
+    ...
+
 class Table:
-    def __init__(self, name: str, db: "SqlDatabase", *, created: t.Optional[bool] = None) -> None:
+    def __init__(self, name: str, db: "SqliteDatabase", *, created: t.Optional[bool] = None) -> None:
         with db.create_connection() as con:
             self.db = db
             cur = con.cursor()
@@ -39,7 +39,9 @@ class Table:
                 return False
     
     @property
-    def rows(self) -> t.Dict[t.Any, t.Any]:
+    def rows(self) -> t.Optional[t.Dict[t.Any, t.Any]]:
+        if not self.exists:
+            return None
         tcolumns = [x[1] for x in self._table_info]
         data = {x: {tcolumns[z]: y[z] for z, _ in enumerate(tcolumns)} for x, y in enumerate(self._rows, start=1)}
         data["_types"] = {tcolumns[x]: y if y else "UNDEFINED" for x, y in enumerate([x[2] for x in self._table_info])} # type: ignore
@@ -47,11 +49,15 @@ class Table:
         return data
 
     @property
-    def columns(self) -> t.Dict[t.Any, t.Any]:
-        return {x[1]: x[2] if x[2] else "UNDEFINED" for x in self._table_info}
+    def columns(self) -> t.Optional[t.Dict[t.Any, t.Any]]:
+        if not self.exists:
+            return None
+        return {int(x[0]) + 1: {x[1]: x[2] if x[2] else "UNDEFINED"} for x in self._table_info}
     
     @property
-    def pretty_print(self) -> str:
+    def pretty_print(self) -> t.Optional[str]:
+        if not self.exists:
+            return None
         columns = "0. | " + " | ".join([x[1] + f": {x[2] if x[2] else 'UNDEFINED'}" for x in self._table_info]) + " |\n"
         for x, row in enumerate(self._rows):
             columns += f"{x + 1}. | " + f"{' | '.join([str(x) for x in row])}" + " |\n"
@@ -85,32 +91,46 @@ class DataBaseResponse:
     def value(self) -> xResponseValueT:
         return self.__value
 
-class SqlDatabase:
-    def __init__(self, dbpath: t.Optional[str] = None, aespwd: t.Optional[str] = None) -> None:
-        self.aes = False
+class SqliteDatabase:
+    def __init__(
+        self, 
+        dbpath: t.Optional[str] = None,
+        datamode: t.Literal["b64", "aes", "default"] = "default",  
+        aespwd: t.Optional[str] = None
+    ) -> None:
+        self.datamode = datamode.lower()
+        assert self.datamode in ["b64", "aes", "default"], "Mode must be either b64 or AES or default."
+        if self.datamode == "aes" and not aespwd:
+            raise ValueError("AES mode requires a data-encryption password.")
+        self.aespwd = aespwd if self.datamode == "aes" else None
         self.dbpath = dbpath if dbpath else "sqlite.db"
-        if aespwd is not None and isinstance(aespwd, str):
-            self.aes = True
-            self.pwd = md5(aespwd.encode()).digest()
+        if self.datamode == "aes" and self.aespwd is not None:
+            self.pwd = md5(self.aespwd.encode()).digest()
 
     def create_connection(self, **kwargs) -> sqlite3.Connection:
-        """
-        Create a connection for a given database with sqlite3.Row row factory
-        """
         conn = sqlite3.connect(self.dbpath, **kwargs)
         conn.row_factory = sqlite3.Row
         return conn
     
-    def _aes_encrypt(self, data: str) -> str:
-        return base64.b64encode(AES.new(self.pwd, AES.MODE_GCM, self.pwd).encrypt(data.encode())).decode('ascii')
+    def _encode(self, data: str) -> str:
+        if self.datamode == "aes":
+            return base64.b64encode(AES.new(self.pwd, AES.MODE_GCM, self.pwd).encrypt(data.encode())).decode('ascii')
+        elif self.datamode == "b64":
+            return base64.b64encode(data.encode()).decode("ascii")
+        else:
+            return data
     
-    def _aes_decrypt(self, data: str) -> str:
-        try:
-            return AES.new(self.pwd, AES.MODE_GCM, self.pwd).decrypt(base64.b64decode(data.encode("ascii"))).decode()
-        except UnicodeDecodeError as e:
-            raise RuntimeError(f'Invalid AES decryption password. {e.__class__.__name__}')
-        except Exception:
-            raise
+    def _decode(self, data: str) -> str:
+        if self.datamode == "aes":
+            try:
+                key = AES.new(self.pwd, AES.MODE_GCM, self.pwd)
+                return key.decrypt(base64.b64decode(data.encode("ascii"))).decode()
+            except UnicodeDecodeError as e:
+                raise RuntimeError(f'Invalid AES decryption password. {e.__class__.__name__}')
+        elif self.datamode == "b64":
+            return base64.b64decode(data.encode()).decode("ascii")
+        else:
+            return data
 
     @property
     def tables(self) -> t.List[Table]:
@@ -134,9 +154,6 @@ class SqlDatabase:
         return Table(name, self).drop()
     
     def execute(self, query: str) -> sqlite3.Cursor:
-        """
-        Executes Custom SQL query
-        """
         with self.create_connection() as con:
             cur = con.cursor()
             cur.execute(query)
@@ -146,20 +163,17 @@ class SqlDatabase:
         self, 
         data: t.Dict[str, t.Any], 
         table: str, 
-        mode: t.Literal[FetchMode.FETCH_ONE, FetchMode.FETCH_ALL] = FetchMode.FETCH_ONE
+        mode: int = FetchMode.FETCH_ONE
     ) -> DataBaseResponse:
         with self.create_connection() as con:
-            if self.aes:
-                data = {x: self._aes_encrypt(str(y)) for x, y in data.items()}
+            data = {x: self._encode(y) for x, y in data.items()}
             cur = con.cursor()
             condition = " AND ".join([f"{x}=?" for x in data.keys()])
             sql = f"SELECT * FROM {table}{' WHERE ' + condition + ';' if data != {} else ';'}"
             cur.execute(sql, tuple([x for x in data.values()]))
-            result = [dict(x) for x in cur.fetchall()]
-            if self.aes:
-                result = [{z: int(self._aes_decrypt(y)) if Table(table, db=self).columns.get(z) == "INT" else self._aes_decrypt(y) for z, y in x.items()} for x in result]
+            result = [{z: self._decode(y) for z, y in x.items()} for x in [dict(x) for x in cur.fetchall()]]
             result = result[0] if mode == 1 and result else result
-        return DataBaseResponse(status=bool(result), value=result if result else None)
+        return DataBaseResponse(status=not not result, value=result if result else None)
     
     def remove(
         self, 
@@ -173,12 +187,13 @@ class SqlDatabase:
                     self.remove(data=x, table=table, limit=limit)
                 return DataBaseResponse(status=True, value=len(data))
             else:
+                data = {x: self._encode(y) for x, y in data.items()}
                 cur = con.cursor()
                 condition = " AND ".join([f"{x}=?" for x in data.keys()])
                 sql = f"DELETE FROM {table} {'WHERE ' + condition if data != {} else ''}" + (f" LIMIT {limit};" if limit else ";")
                 cur.execute(sql, tuple([x for x in data.values()]))
                 con.commit()
-        return DataBaseResponse(status=bool(cur.rowcount), value=cur.rowcount if cur.rowcount else None)
+        return DataBaseResponse(status=not not cur.rowcount, value=cur.rowcount if cur.rowcount else None)
     
     def add(
         self, 
@@ -188,21 +203,21 @@ class SqlDatabase:
         with self.create_connection() as con:
             cur = con.cursor()
             raw_data = data
-            if self.aes:
-                if isinstance(data, dict):
-                    data = {x: self._aes_encrypt(str(y)) for x, y in data.items()}
-                else: # isinstance(data, list)
-                    data = [{z: self._aes_encrypt(str(y)) for z, y in x.items()} for x in data]
-            if isinstance(data, list):
-                values = f"{str([tuple([x for x in x.values()]) for x in data])[1:-1]};".replace(",)", ")")
-            else: # isinstance(data, dict)
+            if isinstance(data, dict):
+                data = {x: self._encode(y) for x, y in data.items()}
                 values = f"{str(tuple([x for x in data.values()]))};".replace(",)", ")")
+            else: # isinstance(data, list)
+                data = [{z: self._encode(y) for z, y in x.items()} for x in data]
+                values = f"{str([tuple([x for x in x.values()]) for x in data])[1:-1]};".replace(",)", ")")
+
             columns = Table(table, db=self).columns
-            columns = f"({', '.join(list(Table(table, db=self).columns.keys()))})"
-            sql = f"INSERT INTO {table} {columns} VALUES {values}"
-            cur.execute(sql)
-            con.commit()
-            return DataBaseResponse(status=bool(cur.rowcount), value=raw_data)
+            if columns:
+                columns = f"({', '.join([list(x.keys())[0] for _, x in columns.items()])})"
+                sql = f"INSERT INTO {table} {columns} VALUES {values}"
+                cur.execute(sql)
+                con.commit()
+                return DataBaseResponse(status=not not cur.rowcount, value=raw_data)
+            raise DataBaseException(f"Table {table} does not exist")
     
     def update(
         self, 
@@ -212,14 +227,13 @@ class SqlDatabase:
         limit: t.Optional[int] = None
     ) -> DataBaseResponse:
         with self.create_connection() as con:
-            if self.aes:
-                to_replace = {x: self._aes_encrypt(str(y)) for x, y in to_replace.items()}
-                data = {x: self._aes_encrypt(str(y)) for x, y in data.items()}
+            to_replace = {x: self._encode(y) for x, y in to_replace.items()}
+            data = {x: self._encode(y) for x, y in data.items()}
             values = ", ".join([f"{x} = {y}" if isinstance(y, int) else f"{x} = '{y}'" for x, y in data.items()])
             if values:
                 cur = con.cursor()
                 condition = " AND ".join([f"{x}=?" for x in to_replace.keys()])
                 sql = f"UPDATE {table} SET {values}{' WHERE ' + condition if condition else ''}{f' LIMIT {limit}' if limit else ''};"
                 cur.execute(sql, tuple([x for x in to_replace.values()]))
-                return DataBaseResponse(status=bool(cur.rowcount), value=cur.rowcount)
+                return DataBaseResponse(status=not not cur.rowcount, value=cur.rowcount)
             raise DataBaseException("Empty data to replace")
